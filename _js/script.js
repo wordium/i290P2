1a9772d85049421ce8a4268e7074d5154118a94f
Sandra Helsley <syh@ischool.berkeley.edu>
2013-10-04 18:16:08 -0700

    Added jQuery.

--- _js/script.js
+++ _js/script.js
@@ -1,2 +1,6 @@
 var KEY = 'apikey=bxyrp4av33rguwg65v78tyhr'; // Rotten Tomatoes API key. Documentation here: http://developer.rottentomatoes.com/docs
 var BASEURL = 'http://api.rottentomatoes.com/api/public/v1.0/'; // Base URL for making calls to the RT API.
+
+$(document).ready(function() {
+
+}
